d712e1f2dfd2106acdcdd5f78fe4516c38977cac
Guillaume Sere <guillaumesere@hotmail.fr>
2022-08-13 18:49:35 +0200

    modif images

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -20,7 +20,7 @@ const Nav = () => {
       <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}>
         <BiBook />
      </a>
-      <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}>
+      <a href="#testimonials" onClick={() => setActiveNav('#testimonials')} className={activeNav === '#testimonials' ? 'active' : ''}>
         <RiServiceLine />
      </a>
       <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}>
