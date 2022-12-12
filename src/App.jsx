26aef7b084cb16500f0e5823c56197e5db446578
Guillaume Sere <guillaumesere@hotmail.fr>
2022-12-12 15:06:16 +0100

    add theme dark

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/header/Header';
 import Nav from './components/nav/Nav';
 import About from './components/about/About';
@@ -8,11 +8,35 @@ import Portfolio from './components/portfolio/Portfolio';
 import Testimonials from './components/testimonials/Testimonials';
 import Contact from './components/contact/Contact';
 import Footer from './components/footer/Footer';
-import Snowfall from 'react-snowfall'
+import Snowfall from 'react-snowfall';
+import DayNightToggle from 'react-day-and-night-toggle';
 
 const App = () => {
+
+    const [isDarkMode, setIsDarkMode] = useState(localStorage.getItem('data-theme') === 'dark' ? true : false)
+
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
+      const newColorScheme = e.matches ? 'dark' : 'light'
+  
+      setIsDarkMode(newColorScheme === 'dark' ? true : false)
+      localStorage.setItem('data-theme', newColorScheme)
+      document.body.setAttribute('data-theme', localStorage.getItem('data-theme'))
+    })
+  
+    const handleChangeTheme = () => {
+      setIsDarkMode(!isDarkMode)
+      if(!isDarkMode) {
+        localStorage.setItem('data-theme', 'dark')
+        document.body.setAttribute('data-theme', 'dark')
+      } else {
+        localStorage.setItem('data-theme', 'light')
+        document.body.setAttribute('data-theme', 'light')
+      }
+    }
+
     return (
         <>
+            <DayNightToggle className='dark' size={40} onChange={handleChangeTheme} checked={isDarkMode} />
             <Snowfall style={{ position: 'fixed',width: '100vw', height: '100vh'}} />
             <Header />
             <Nav />
