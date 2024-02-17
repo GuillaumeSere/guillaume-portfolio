d2e216bd8600173fcd87d57fc8ce81623123cf91
Guillaume Sere <guillaumesere@hotmail.fr>
2024-02-17 13:53:23 +0100

    refactoring

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import Modal from 'react-modal';
 
+
+// Définir l'élément racine de l'application
+Modal.setAppElement('#root');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
