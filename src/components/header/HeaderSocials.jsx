e7e5a71b637fc4dd888cbd28497b41d8d3346b25
Guillaume Sere <guillaumesere@hotmail.fr>
2024-05-10 18:48:59 +0200

    ajout liens

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import {BsLinkedin} from 'react-icons/bs';
 import {FaGithub} from 'react-icons/fa';
 import { FaNpm } from "react-icons/fa";
+import mentor from "../../assets/mentor.png";
 
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-      <a href="https://www.linkedin.com/in/guillaume-s%C3%A9r%C3%A9-2019a7116/" target="_blank" title="Connectez-vous avec moi sur LinkedIn"><BsLinkedin /></a>
-      <a href="https://github.com/GuillaumeSere" target="_blank" title="Explorez mes projets sur GitHub"><FaGithub /></a>
-      <a href="https://www.npmjs.com/~guillaumesere" target="_blank" title="Visitez mon profil NPM"><FaNpm className='npm' /></a>
+      <a href="https://www.linkedin.com/in/guillaume-s%C3%A9r%C3%A9-2019a7116/" target="_blank" rel="noreferrer" title="Connectez-vous avec moi sur LinkedIn"><BsLinkedin /></a>
+      <a href="https://github.com/GuillaumeSere" target="_blank" rel="noreferrer" title="Explorez mes projets sur GitHub"><FaGithub /></a>
+      <a href="https://www.npmjs.com/~guillaumesere" target="_blank" rel="noreferrer" title="Visitez mon profil NPM"><FaNpm className='npm' /></a>
+      <a href="https://www.frontendmentor.io/profile/GuillaumeSere" target="_blank" rel="noreferrer" title="Visitez mon profil Frontend Mentor"><img src={mentor} className='mentor' alt="" /></a>
     </div>
   )
 }
