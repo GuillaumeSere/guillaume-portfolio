0cd1a4dc1f11b6b5d92459433ea20b0438417f96
Guillaume Sere <guillaumesere@hotmail.fr>
2024-07-24 12:44:34 +0200

    modif liens

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -7,7 +7,7 @@ import mentor from "../../assets/mentor.png";
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-      <a href="https://www.linkedin.com/in/guillaume-s%C3%A9r%C3%A9-2019a7116/" target="_blank" rel="noreferrer" title="Connectez-vous avec moi sur LinkedIn"><BsLinkedin /></a>
+      <a href="https://www.linkedin.com/in/guillaume-séré/" target="_blank" rel="noreferrer" title="Connectez-vous avec moi sur LinkedIn"><BsLinkedin /></a>
       <a href="https://github.com/GuillaumeSere" target="_blank" rel="noreferrer" title="Explorez mes projets sur GitHub"><FaGithub /></a>
       <a href="https://www.npmjs.com/~guillaumesere" target="_blank" rel="noreferrer" title="Visitez mon profil NPM"><FaNpm className='npm' /></a>
       <a href="https://www.frontendmentor.io/profile/GuillaumeSere" target="_blank" rel="noreferrer" title="Visitez mon profil Frontend Mentor"><img src={mentor} className='mentor' alt="" /></a>
